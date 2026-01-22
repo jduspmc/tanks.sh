444dd84d7713c5930936391cd70bd2c9c20bb5b0
jduspmc <juan.uribe.physics@gmail.com>
2026-01-22 00:38:09 -0500

    add movement and info update

--- tanks.sh
+++ tanks.sh
@@ -1,26 +1,13 @@
 #!/usr/bin/env bash
 
-####
-# Functions
-
-# Clean up screen at exit
-cleanup() {
-    stty sane  # restore stty
-    tput cnorm # restore cursor
-    tput rmcup # go back to primary screen
-}
-
-# prepare screen
-tput smcup                      # switch to alternate screen
-stty -echo -icanon min 0 time 0 # change terminal behaviour (no echo and no enter to read)
-tput civis                      # hide cursor
-trap cleanup EXIT INT TERM
+##########
+# source files
 
 # Source tput wrapper from https://github.com/bahamas10/bash-tput
 . ./tput
 
-###
-# some constants
+##########
+# constants
 
 # color constants
 COL_L_TANK=$'\x1b[38;5;28m'
@@ -32,12 +19,14 @@ COL_R_TANK=$'\x1b[38;5;124m'
 # col_trail3=
 COL_NONE=$'\x1b[0m'
 
-###
+##########
+# global variables
+
 # player turn: true for player 1, false for player 2.
 player=true
 
 # find playable size
-declare -i HEIGHT=$(($(tput lines) - 3)) WIDTH=$(($(tput cols) - 2))
+declare HEIGHT=$(($(tput lines) - 3)) WIDTH=$(($(tput cols) - 2))
 
 #here logic when screen too small
 # Set the columns and rows of the playable space (figure out how to make dynamic sigwinch)
@@ -48,8 +37,8 @@ declare -i HEIGHT=$(($(tput lines) - 3)) WIDTH=$(($(tput cols) - 2))
 #     COLS=$(tput cols)
 # }
 
-left_tank_pos=(1 $((HEIGHT - 4)))
-right_tank_pos=($((WIDTH - 14)) $((HEIGHT - 4)))
+left_tank_pos=(1 $((HEIGHT - 5)))
+right_tank_pos=($((WIDTH - 14)) $((HEIGHT - 5)))
 
 IFS= read -r -d '' L_TANK <<-"EOF"
      __
@@ -57,6 +46,7 @@ IFS= read -r -d '' L_TANK <<-"EOF"
 __/_______\__
 \O_O_O_O_O_O/
 EOF
+L_TANK=$COL_L_TANK$L_TANK$COL_NONE
 
 IFS= read -r -d '' R_TANK <<-"EOF"
       __
@@ -64,6 +54,7 @@ IFS= read -r -d '' R_TANK <<-"EOF"
 __/_______\__
 \O_O_O_O_O_O/
 EOF
+R_TANK=$COL_R_TANK$R_TANK$COL_NONE
 
 # special="⋅"
 
@@ -71,95 +62,123 @@ EOF
 # ⋅⋅⋅⋅⋅
 #  ⋅⋅⋅
 
-###
+##########
 # initial state
 l_angle=45
 r_angle=45
+l_power=100
+r_power=100
 
+##########
+# Functions
+
+# Clean up screen at exit
+cleanup() {
+    stty sane  # restore stty
+    tput cnorm # restore cursor
+    tput rmcup # go back to primary screen
+}
+
+# draw a tank
 draw-tank() {
     local x=$1
     local y=$2
-    local tank="$3"
-
+    local tank=$3
     while IFS= read -r line; do
         tput cup "$y" "$x"
         echo -n "$line"
         ((y++))
-    done < <(printf '%s\n' "$tank")
+    done <<<"$tank"
 }
 
-draw-left-tank() {
-    read -r x y <<<"${left_tank_pos[@]}"
-    while IFS= read -r line; do
-        tput cup "$y" "$x"
-        echo -n "$COL_L_TANK$line$COL_NONE"
-        ((y++))
-    done <<<"$L_TANK"
+# deletes a tank to draw a new one
+delete-tank() {
+    local x=$1
+    local y=$2
+    local tank=$3
+    local lines=4 # number of lines in tank
+
+    # overwrite each line with spaces
+    local i
+    for ((i = 0; i < lines; i++)); do
+        tput cup $((y + i)) "$x"
+        printf '%*s' 13 "" # print enough spaces
+    done
 }
 
-draw-right-tank() {
-    read -r x y <<<"${right_tank_pos[@]}"
-    while IFS= read -r line; do
-        tput cup "$y" "$x"
-        echo -n "$COL_R_TANK$line$COL_NONE"
-        ((y++))
-    done <<<"$R_TANK"
+# draw projectile info
+draw-info() {
+    local x=$2
+    local angle=$1
+    tput cup $((HEIGHT - 1)) "$x"
+    echo -n "Angle: $angle° - Power: 100"
 }
 
+# move a tank to the right
 move-tank-right() {
-    if [[ $player ]]; then
+    if $player; then
+        delete-tank "${left_tank_pos[@]}" "$L_TANK"
         ((left_tank_pos[0]++))
+        draw-tank "${left_tank_pos[@]}" "$L_TANK"
     else
+        delete-tank "${right_tank_pos[@]}" "$R_TANK"
         ((right_tank_pos[0]++))
+        draw-tank "${right_tank_pos[@]}" "$R_TANK"
     fi
 }
 
+# move a tank to the left
 move-tank-left() {
     if $player; then
+        delete-tank "${left_tank_pos[@]}" "$L_TANK"
         ((left_tank_pos[0]--))
+        draw-tank "${left_tank_pos[@]}" "$L_TANK"
     else
+        delete-tank "${right_tank_pos[@]}" "$R_TANK"
         ((right_tank_pos[0]--))
+        draw-tank "${right_tank_pos[@]}" "$R_TANK"
     fi
 }
 
-tput clear
+##########
+# main game logic
 
-tput cup 0 0
-echo "0,0"
-tput cup "$HEIGHT" 0
-echo "0,$HEIGHT"
-tput cup 0 "$WIDTH"
-echo "0,$WIDTH"
+# prepare screen
+tput smcup                      # switch to alternate screen
+stty -echo -icanon min 0 time 0 # change terminal behaviour (no echo and no enter to read)
+tput civis                      # hide cursor
+trap cleanup EXIT INT TERM
+
+tput clear # clear to screen to star game
+
 tput cup "$HEIGHT" "$WIDTH"
 echo "$WIDTH,$HEIGHT"
 
-while true; do
-    draw-tank "${left_tank_pos[@]}" "$COL_L_TANK$L_TANK$COL_NONE"
-    draw-tank "${right_tank_pos[@]}" "$COL_R_TANK$R_TANK$COL_NONE"
+# draws initial state
+draw-tank "${left_tank_pos[@]}" "$L_TANK"
+draw-tank "${right_tank_pos[@]}" "$R_TANK"
 
-    # draw-left-tank
-    # draw-right-tank
+while true; do
+    draw-info "$l_angle" "0"
+    draw-info "$r_angle" $((WIDTH - 22))
+
     read -rsn1 key
+    if [[ $key == $'\e' ]]; then
+        read -rsn2 key2
+        key+="$key2"
+    fi
+
     case "$key" in
-    a)
-        move-tank-left
-        draw-left-tank
-        ;;
-    d)
-        move-tank-right
-        draw-left-tank
-        ;;
+    a | $'\x1b[D') move-tank-left ;;
+    d | $'\x1b[C') move-tank-right ;;
+    w | $'\x1b[A') if $player; then ((l_angle++)); else ((r_angle++)); fi ;;
+    s | $'\x1b[B') if $player; then ((l_angle--)); else ((r_angle--)); fi ;;
+    m) if $player; then ((l_power++)); else ((r_power++)); fi ;;
+    l) if $player; then ((l_power--)); else ((r_power--)); fi ;;
+    ' ') ;; # fire!
     q) break ;;
     *) continue ;;
     esac
 done
 
-# sleep 10
-
 cleanup
-
-# echo -n "$ESC[25;25H"
-# echo -n "$L_TANK"
-
-# echo
-C
