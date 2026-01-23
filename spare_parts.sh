35e69ce283dd29667e9cadf8d13aa0706714b46e
jduspmc <juan.uribe.physics@gmail.com>
2026-01-22 22:11:39 -0500

    more spare parts

--- spare_parts.sh
+++ spare_parts.sh
@@ -15,3 +15,26 @@
 #         ((y++))
 #     done <<<"$R_TANK"
 # }
+# pdraw_obstacles_frame() {
+#     local i j row frame=""
+#     local minx=$area1
+#     local width=$((area2 - area1 + 1))
+#
+#     for ((j = 1; j <= height; j++)); do
+#         row=""
+#         for ((i = area1; i <= area2; i++)); do
+#             if ((RANDOM % 3 == 0)); then
+#                 obstacles["$i,$j"]="X"
+#                 row+="X"
+#             else
+#                 unset 'obstacles["$i,$j"]'
+#                 row+=" "
+#             fi
+#         done
+#         frame+="$row"$'\n'
+#     done
+#
+#     tput cup 1 "$area1"
+#     printf '%b' "$frame"
+# }
+#
